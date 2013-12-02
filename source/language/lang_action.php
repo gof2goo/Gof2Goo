af89b71d453792f5dc669683bfffe7ad05df269a
m-hmt <m.hemati@live.com>
2013-12-02 17:32:52 +0330

    قدم اول --- اولین  سپارش
    کپی از نسخه اصلی
    گفتگو سیستم 3.1

---
+++ source/language/lang_action.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ *      [Discuz!] (C)2001-2099 Comsenz Inc.
+ *      This is NOT a freeware, use is subject to license terms
+ *
+ *      $Id: lang_action.php 27449 2012-02-01 05:32:35Z zhangguosheng $
+ */
+
+if(!defined('IN_DISCUZ')) {
+	exit('Access Denied');
+}
+
+$lang = array
+(
+	0 => 'ثبت نام/ورود',
+	1 => 'پرفایل',
+	2 => 'انجمن',
+	3 => 'گروه',
+	4 => 'خانه',
+
+	100 => 'موارد دیگر',
+	127 => 'افزونه',
+
+);
+
+?>
